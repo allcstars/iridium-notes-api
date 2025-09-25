cb62ec1c108ca2e4085c6fe4f34b45d12ccbd9e3
allcstars <allcstars@proton.me>
2025-09-25 15:08:01 -0500

    Implement @CurrentUser decorator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,19 @@
 import { Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 
 @Controller('user')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('info')
-  async getInfo(id: string) {
+  async getInfo(@CurrentUser('sub') id: string) {
     return this.usersService.findById(id);
   }
 
   @Post('info')
   async updateInfo(
+    @CurrentUser('sub')
     id: string,
     email?: string,
     password?: string,
